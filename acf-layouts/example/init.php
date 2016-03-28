f907bb8c83a06f9a98be00e5b970dfb5bbf3c945
Eric Wennerberg <ericwenn@gmail.com>
2016-03-28 19:23:46 +0200

    Commit-fix

--- acf-layouts/example/init.php
+++ acf-layouts/example/init.php
@@ -1,6 +1,10 @@
 <?php
+	/*
+		This file will be included when a module with the name 'example' is found by ACFListener.
+		All the modules fields will be available in 'get_sub_field'
+		
+	*/
 	require_once('ACF_Example.php');
-
 
 	$example = new ACF_Example();
 
